eeea3d0a5595d5623c131982c7a2fb8c15ad9a1e
BobElManetes <f8sbdktvng@privaterelay.appleid.com>
2026-05-21 17:57:53 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Joc Guerra Civil',
+  description: 'Per Marcel, Asier, Martí, Eloi',
+  generator: 'Personal',
   icons: {
     icon: [
       {
